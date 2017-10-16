54563051b90647447c5615b1f617f7aa3b11deea
Isaac Tullis <isaacjtullis@gmail.com>
2017-10-16 11:49:30 -0500

    launching files

--- app/app.modules.ts
+++ app/app.modules.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './component.app';
 
+// decorators modify javascript classes
 @NgModule({
+  // imports modules we will need, features for your app
   imports: [
     BrowserModule
   ],
+  // declarations will allow us to import all the modules we need into our app
   declarations: [
     AppComponent
   ],
+  // this is the main container for the entire app (THE ROOT)
   bootstrap: [
     AppComponent
   ]
